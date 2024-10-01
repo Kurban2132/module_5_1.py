989710b40b9415e81fee6b84919ed53d82802782
Kurban2132 <k.kokanov@yandex.ru>
2024-10-01 15:47:03 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -7,11 +7,18 @@ class House():
 
     def go_to(self, new_floor):
         print(new_floor)
+
+        for go_to in range(1, new_floor):
+            print(go_to)
+            
         if new_floor > self.number_of_floors or new_floor < 1:
+            print(1, new_floor)
+
+        else:
             print('"Такого этажа не существует"')
 
-        for go_to in range(new_floor + 1):
-            print(go_to)
+
+
 
 
 h1 = House('ЖК Горский', 18)
