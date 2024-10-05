5de396b95252e7d0e37c83f79ee65d4ae57181e5
Kurban2132 <k.kokanov@yandex.ru>
2024-10-05 17:10:26 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -1,31 +1,29 @@
-class House():                                      #Создаем класс House
-    def __init__(self, name, number_of_floors):     #Создаем на основе класса House экземпляр.
+class House():
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = number_of_floors    #Вывод домов с этажами
+        self.number_of_floors = number_of_floors
         print(name, number_of_floors)
-
 
 
     def go_to(self, new_floor):
         floor = 0
-        print(f'Дом {self.name} имеет {self.number_of_floors} этажей \n Поднимаемся на {new_floor}-й')
+        print(new_floor)
+
+        print(f"Дом {self.name} имеет {self.number_of_floors} этажей \n Поднимаемся на {new_floor} - й")
 
         if new_floor > self.number_of_floors or new_floor < 1:
-            print(f'{new_floor}"Такого этажа не существует"')
+            print(floor + 1)
+
+
         else:
             for floor in range(new_floor):
-                print(floor + 1)
-        
-            
-
+                print(f"{new_floor + 1} - Такого этажа не существует")
 
 
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
-
-
+h1.go_to(18)
+h2.go_to(2)
 
